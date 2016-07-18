0c7252ca15faf89e3a244b09bcc226ae3d79763f
Christian Peter <christian.peter@knowit.no>
2016-07-18 16:24:11 +0200

    Fixes small linting erros NDLA-357

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,14 @@
 import React, { PropTypes } from 'react';
 import { titlesI18N } from '../util/i18nFieldFinder';
 import Script from './Script';
-const Content = ({data, parsedContent, lang}) => {
-  return (
-    <div className="content">
-      <h1>{titlesI18N(data, lang, true)}</h1>
-      <div dangerouslySetInnerHTML={{__html: parsedContent}} />
-      {data.requiredLibraries ? <Script requiredLibraries={data.requiredLibraries} /> : ''}
-    </div>
-  );
-};
+const Content = ({data, parsedContent, lang}) => (
+  <div className="content">
+    <h1>{titlesI18N(data, lang, true)}</h1>
+    <div dangerouslySetInnerHTML={{__html: parsedContent}} />
+    {data.requiredLibraries ? <Script requiredLibraries={data.requiredLibraries} /> : ''}
+  </div>
+);
+
 
 Content.propTypes = {
   data: PropTypes.object.isRequired,
