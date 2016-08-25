f7fe06ad1e501359c44807ce67c033d28f99f81c
Henrik Nårstad <henrik.narstad@knowit.no>
2016-08-25 12:47:48 +0200

    NDLA-415: Removed unused style.css

--- server/Html.js
+++ server/Html.js
@@ -1,10 +1,8 @@
 import React, { PropTypes } from 'react';
 const Html = ({component}) => {
-  const styleUrl = '/content-frontend/assets/style.css';
   return (
     <html lang="en-us">
       <head>
-        <link rel="stylesheet" type="text/css" href={styleUrl} />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,300italic,300|Signika:400,600,300,700" />
         <link rel="shortcut icon" href="/content-frontend/assets/favicon.ico" type="image/x-icon" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
