c516c693fd80a63654c219163fed7e67379eb8ed
Øyvind Marthinsen <oma@knowit.no>
2016-08-30 14:35:43 +0200

    Add ava test runner

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,17 @@ module.exports = {
       experimentalObjectRestSpread: true
     }
   },
+  env: {
+    node: true
+  },
   rules: {
     "max-len": [2, 200, 2, {
       ignoreUrls: true,
       ignoreComments: false
     }],
     'react/jsx-filename-extension': [0, { extensions: [".js", ".jsx"] }],
+    'import/no-extraneous-dependencies':
+      ['error', {'devDependencies': true}],
     'react/prop-types': [ 2, {'ignore': ['children','className'] }],
     'import/prefer-default-export': 0
   }
