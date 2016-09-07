aef0a369dc22cb585c374955f802402a361a863e
Øyvind Marthinsen <oma@knowit.no>
2016-09-07 22:36:43 +0200

    Fix linting for async / await

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  parser: "babel-eslint",
   extends: "airbnb",
   parserOptions: {
     ecmaFeatures: {
@@ -8,11 +9,22 @@ module.exports = {
   env: {
     node: true
   },
+  plugins: [
+    "babel"
+  ],
   rules: {
     "max-len": [2, 200, 2, {
       ignoreUrls: true,
       ignoreComments: false
     }],
+
+    // generator-star-spacing breaks linting in codebases that use async/await syntax
+    // See https://github.com/eslint/eslint/issues/6528
+    // enable babel/generator-star-spacing instead.
+    'generator-star-spacing': 0,
+    'babel/generator-star-spacing': [2, { 'before': false, 'after': true }],
+
+
     'react/jsx-filename-extension': [0, { extensions: [".js", ".jsx"] }],
     'import/no-extraneous-dependencies':
       ['error', {'devDependencies': true}],
