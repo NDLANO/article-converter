0ffe9a585729436d67a7a49c23ab8d8b5ee8bfc3
Øyvind Marthinsen <oma@knowit.no>
2016-09-07 16:02:55 +0200

    Default to 100% width and height for h5p content

--- src/figures/H5P.js
+++ src/figures/H5P.js
@@ -8,11 +8,34 @@
 
 import React, { PropTypes } from 'react';
 
-const H5P = ({ h5p }) => (
-  <figure>
-    <iframe src={h5p.url} width="400" height="300" />
-  </figure>
-);
+const H5P = ({ h5p }) => {
+  const outerDivStyle = {
+    display: 'block',
+    position: 'relative',
+    maxWidth: '100%',
+  };
+  const innerDivStyle = {
+    paddingTop: '100%',
+  };
+  const iframeStyle = {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: '0',
+    bottom: '0',
+    right: '0',
+    left: '0',
+  };
+  return (
+    <div style={outerDivStyle}>
+      <div style={innerDivStyle}>
+        <figure>
+          <iframe style={iframeStyle} src={h5p.url} />
+        </figure>
+      </div>
+    </div>
+  );
+};
 
 H5P.propTypes = {
   h5p: PropTypes.object.isRequired,
