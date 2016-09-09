9c3271625e7df6766a9fad668346bf6983d7056e
Øyvind Marthinsen <oma@knowit.no>
2016-09-09 09:17:20 +0200

    Add generator test

---
+++ src/__tests__/generator-test.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2016-present, NDLA.
+ *
+ * This source code is licensed under the GPLv3 license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import test from 'ava';
+import articleContent from './_articleContentTestData';
+import { getFigures } from '../generator';
+
+test('generator getFigures', async t => {
+  const figures = await getFigures(articleContent);
+  t.is(figures.length, 5);
+  t.deepEqual(figures, [{ id: 8,
+    resource: 'h5p',
+    figure: { url: 'http://ndla.no/h5p/embed/163489' } },
+  { id: 7,
+    resource: 'image',
+    size: 'hovedspalte',
+    url: 'http://api.test.ndla.no/images/1327' },
+  { id: 6,
+    resource: 'image',
+    size: 'hovedspalte',
+    url: 'http://api.test.ndla.no/images/1326' },
+  { id: 4,
+    resource: 'image',
+    size: 'hovedspalte',
+    url: 'http://api.test.ndla.no/images/1325' },
+  { id: 2,
+    resource: 'brightcove',
+    figure:
+     { account: 4806596774001,
+       player: 'BkLm8fT',
+       videoid: 'ref:46012' } }]);
+  t.pass();
+});
