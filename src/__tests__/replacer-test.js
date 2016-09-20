2befc5abe8d1c415344d88666ae2b6f698c707ee
Øyvind Marthinsen <oma@knowit.no>
2016-09-20 11:59:12 +0200

    Extend replacer test to include content-link resource

--- src/__tests__/replacer-test.js
+++ src/__tests__/replacer-test.js
@@ -16,6 +16,7 @@ test('replacer replaceFiguresInHtml', async t => {
       <figure data-resource="h5p" data-id="8" data-url="http://ndla.no/h5p/embed/163489"></figure>
       <p>SomeText</p>
       <figure data-resource="brightcove" data-id="2" data-videoid="ref:46012" data-account="4806596774001" data-player="BkLm8fT"></figure>
+      <figure data-resource="content-link" data-id="1" data-content-id="425" data-link-text="Valg av informanter"></figure>
     </article>
   `;
 
@@ -30,11 +31,13 @@ test('replacer replaceFiguresInHtml', async t => {
         alttexts: [{ alttext: 'alt', lang: 'nb' }],
         images: { full: { url: 'http://api.test.ndla.no/images/full/421694461_818fee672d_o.jpg' } } },
     },
+    { id: 1, resource: 'content-link', contentId: '425', linkText: 'Valg av informanter' },
     { id: 2, resource: 'brightcove', figure: { account: 4806596774001, player: 'BkLm8fT', videoid: 'ref:46012' } }];
 
   const replaced = await replaceFiguresInHtml(figures, articleContent, 'nb');
 
   t.truthy(replaced.indexOf('<figure><img alt="presentation" src="http://api.test.ndla.no/images/full/421694461_818fee672d_o.jpg"/><span class="figure_caption">alt</span></figure>') !== -1);
   t.truthy(replaced.indexOf('<figure><iframe src="http://ndla.no/h5p/embed/163489"></iframe></figure>') !== -1);
+  t.truthy(replaced.indexOf('<a href="http://api.test.ndla.no:8082/nb/article/425">Valg av informanter</a>') !== -1);
   t.truthy(replaced.indexOf('<iframe style="width:100%;height:100%;position:absolute;top:0px;bottom:0px;right:0px;left:0px;" src="//players.brightcove.net/4806596774001/BkLm8fT_default/index.html?videoId=ref:46012" allowfullscreen=""></iframe>') !== -1); // eslint-disable-line max-len
 });
