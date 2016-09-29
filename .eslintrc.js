bdb0c50acc19bf90834c71f3063c66fca8357ae1
Øyvind Marthinsen <oma@knowit.no>
2016-09-29 14:38:47 +0200

    Use ndla eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,12 @@
 module.exports = {
-  extends: "airbnb",
-  parserOptions: {
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true
-    }
-  },
+  extends: "ndla",
   env: {
     node: true
   },
-  plugins: [
-    "babel"
-  ],
   rules: {
     "max-len": [2, 200, 2, {
       ignoreUrls: true,
       ignoreComments: false
     }],
-
-    "react/forbid-prop-types": 0,
-    'react/jsx-filename-extension': [0, { extensions: [".js", ".jsx"] }],
-    'import/no-extraneous-dependencies':
-      ['error', {'devDependencies': true}],
-    'react/prop-types': [ 2, {'ignore': ['children','className'] }],
-    'import/prefer-default-export': 0
   }
 };
