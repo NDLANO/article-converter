a5da767b7408d18dcc5263365f8ecce626481387
Christian Peter <christian.peter@knowit.no>
2016-11-11 10:48:28 +0100

    Removes stupid config addition

--- src/config.js
+++ src/config.js
@@ -19,7 +19,7 @@ module.exports = Object.assign({
   host: process.env.ARTICLE_OEMBED_HOST || 'localhost',
   port: process.env.ARTICLE_OEMBED_PORT || '3000',
   ndlaFrontendUrl: process.env.NDLA_FRONTEND_URL || 'http://api.test.ndla.no:8082',
-  ndlaApiUrl: process.env.NDLA_API_URL || 'http://localhost:20000',
+  ndlaApiUrl: process.env.NDLA_API_URL || 'http://api.test.ndla.no',
 
   app: {
     title: 'NDLA Content frontend',
