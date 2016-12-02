d9ecc4cfabd9ada3da79eb736b40a14ae8e1561f
Christian Peter <christian.peter@knowit.no>
2016-12-02 15:29:43 +0100

    Adds phantomjs plus a counter to find height #123

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import 'isomorphic-fetch';
 import defined from 'defined';
 import express from 'express';
 import compression from 'compression';
+import phantom from 'phantom';
 import cors from 'cors';
 import config from './config';
 import { fetchArticle } from './api/articleApi';
@@ -18,6 +19,7 @@ import { titleI18N, contentI18N, footNotesI18N, introductionI18N } from './utils
 import { htmlTemplate, htmlErrorTemplate } from './utils/htmlTemplates';
 import { transformContentAndExtractCopyrightInfo } from './transformers';
 import { getAppropriateErrorResponse } from './utils/errorHelpers';
+import { computeHeightWithCountingAlgorithm } from './utils/iframeHeight';
 
 const app = express();
 app.use(compression());
@@ -26,10 +28,13 @@ app.use(cors({
   credentials: true,
 }));
 
-async function fetchArticleTitle(articleId, lang) {
+// app.use(__dirname, express.static('assets/css/'));
+app.use('/article-oembed', express.static('public'));
+/* async function fetchArticleTitle(articleId, lang) {
   const article = await fetchArticle(articleId);
   return titleI18N(article, lang, true);
-}
+}*/
+
 
 async function fetchAndTransformArticle(articleId, lang, includeScripts = false) {
   const article = await fetchArticle(articleId);
@@ -37,13 +42,27 @@ async function fetchAndTransformArticle(articleId, lang, includeScripts = false)
   const rawContent = contentI18N(article, lang, true);
   const footNotes = footNotesI18N(article, lang, true);
   const rawIntroduction = introductionI18N(article, lang, true);
+  const title = titleI18N(article, lang, true);
 
   const introduction = rawIntroduction ? rawIntroduction.introduction : '';
   const requiredLibraries = includeScripts ? article.requiredLibraries : [];
   const content = await transformContentAndExtractCopyrightInfo(rawContent, lang, requiredLibraries);
 
 
-  return { ...article, content: content.html, footNotes, contentCopyrights: content.copyrights, introduction };
+  return { ...article, title, content: content.html, footNotes, contentCopyrights: content.copyrights, introduction };
+}
+
+async function computeHeightWithPhantomJs(url, width, articleId, lang) {
+  const instance = await phantom.create();
+  const page = await instance.createPage(['--load-images=no']);
+  await page.open(url);
+  await page.property('viewportSize', { width, height: 400 });
+  const dimensions = await page.evaluate(() => ({
+    scrollHeight: document.documentElement.scrollHeight,
+  }));
+  await instance.exit();
+  const article = await fetchAndTransformArticle(articleId, lang);
+  return { dimensions, article };
 }
 
 app.get('/article-oembed/raw/:lang/:id', (req, res) => {
@@ -64,7 +83,7 @@ app.get('/article-oembed/html/:lang/:id', (req, res) => {
   const articleId = req.params.id;
   fetchAndTransformArticle(articleId, lang, true)
     .then((article) => {
-      res.send(htmlTemplate(lang, article.content));
+      res.send(htmlTemplate(lang, article.content, article.introduction, article.title));
       res.end();
     }).catch((error) => {
       const response = getAppropriateErrorResponse(error, config.isProduction);
@@ -83,15 +102,52 @@ app.get('/article-oembed', (req, res) => {
   const paths = url.split('/');
   const articleId = paths.length > 5 ? paths[5] : paths[4];
   const lang = paths.length > 2 && isValidLocale(paths[3]) ? paths[3] : 'nb';
-  const html = `<iframe src="${config.ndlaApiUrl}/article-oembed/html/${lang}/${articleId}" frameborder="0" />`;
-  fetchArticleTitle(articleId, lang)
-    .then((title) => {
+  const iframeUrl = `http://localhost:3001/article-oembed/html/${lang}/${articleId}`;
+
+  fetchAndTransformArticle(articleId, lang)
+    .then((article) => {
+      const width = req.query.width ? req.query.width : 900;
+      const height = computeHeightWithCountingAlgorithm(article, width);
+      const html = `<iframe src="${iframeUrl}" frameborder="0" style="height:${height}px;"/>`;
       res.json({
         type: 'rich',
         version: '1.0', // oEmbed version
-        height: req.query.height ? req.query.height : 800,
-        width: req.query.width ? req.query.width : 800,
-        title,
+        height,
+        width,
+        title: article.title,
+        html,
+      });
+    }).catch((error) => {
+      const response = getAppropriateErrorResponse(error, config.isProduction);
+      res.status(response.status).json(response);
+    });
+});
+app.get('/article-oembed/phantom', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  const url = req.query.url;
+  if (!url) {
+    res.status(404).json({ status: 404, text: 'Url not found' });
+  }
+
+  const paths = url.split('/');
+  const articleId = paths.length > 5 ? paths[5] : paths[4];
+  const lang = paths.length > 2 && isValidLocale(paths[3]) ? paths[3] : 'nb';
+  const iframeUrl = `http://localhost:3001/article-oembed/html/${lang}/${articleId}`;
+  const apiiframeUrl = `http://api.test.ndla.no/article-oembed/html/${lang}/${articleId}`;
+  const width = req.query.width ? req.query.width : 900;
+  computeHeightWithPhantomJs(apiiframeUrl, width, articleId, lang)
+    .then((data) => {
+      const height = data.dimensions.scrollHeight;
+      const article = data.article;
+      console.log(height);
+      const html = `<iframe src="${iframeUrl}" frameborder="0" style="height:${height}px;"/>`;
+      res.json({
+        type: 'rich',
+        version: '1.0', // oEmbed version
+        height,
+        width,
+        title: article.title,
         html,
       });
     }).catch((error) => {
--- src/utils/htmlTemplates.js
+++ src/utils/htmlTemplates.js
@@ -12,19 +12,23 @@
    <script type="text/javascript">
     window.onload = function(e){
       var data = { context: 'ndla-oembed', height: document.getElementsByTagName("body")[0].offsetHeight};
-      parent.postMessage(data, e.target.referrer);
+      parent.postMessage(data, "*");
     }
    </script>
    `;
- export const htmlTemplate = (lang, body) =>
+ export const htmlTemplate = (lang, body, introduction, title) =>
   `<!doctype html>\n<html lang=${lang} >
     <head>
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <link href="/article-oembed/article.css" rel="stylesheet" type="text/css" >
     </head>
     <body>
+      <h1>${title}</h1>
+      <section>
+        ${introduction}
+      </section>
       ${body}
-      ${findHeightScript}
     </body>
   </html>`;
 
