6bb143828b0e793a119fde9b8b26bba002d0ddca
Øyvind Marthinsen <oma@knowit.no>
2017-06-22 14:18:02 +0200

    Set correct aside classes

--- src/__tests__/transformers-test.js
+++ src/__tests__/transformers-test.js
@@ -21,9 +21,9 @@ test('transformers/asideReplacers changes aside to accommodate frontend styling'
   const result = asideReplacers.reduce((html, f) => f(html), content);
 
   expect(result).toMatch(
-    '<aside class="c-aside u-1/3@desktop"><div class="c-aside__content"><h2>Test1</h2><div>Stuff</div></div><button class="c-button c-aside__button"></button></aside>'
+    '<aside class="c-aside c-aside--float expanded"><div class="c-aside__content"><h2>Test1</h2><div>Stuff</div></div></aside>'
   );
   expect(result).toMatch(
-    '<aside class="c-aside u-1/3@desktop"><div class="c-aside__content"><h2>Test2</h2><div>Other stuff</div></div><button class="c-button c-aside__button"></button></aside>'
+    '<aside class="c-aside c-aside--float expanded"><div class="c-aside__content"><h2>Test2</h2><div>Other stuff</div></div></aside>'
   );
 });
