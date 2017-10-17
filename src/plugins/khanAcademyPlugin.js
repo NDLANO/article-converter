9e9b08e4188161b115326621d6dcf870bda6cfb6
Jonas Natten <jonas.natten@knowit.no>
2017-10-17 12:27:18 +0200

    #655: Misplaced figure tag

--- src/plugins/khanAcademyPlugin.js
+++ src/plugins/khanAcademyPlugin.js
@@ -9,7 +9,7 @@ export default function createKhanAcademyPlugin() {
   const embedToHTML = embed => {
     const { url, width, height } = embed.data;
     embed.embed.replaceWith(
-      `<figure></figure><iframe src="${url}" width="${width}" height="${height}" scrolling="no" frameborder="no" align="center"></iframe></figure>`
+      `<figure><iframe src="${url}" width="${width}" height="${height}" scrolling="no" frameborder="no" align="center"></iframe></figure>`
     );
   };
 
