455c6769a521ef20ac470b31ec741196eb138cda
Øyvind Marthinsen <oma@knowit.no>
2017-12-21 15:39:30 +0100

    Add error handling to external resources

--- src/plugins/externalPlugin.js
+++ src/plugins/externalPlugin.js
@@ -8,14 +8,28 @@
 
 import { fetchOembed } from '../api/oembedProxyApi';
 import { wrapInFigure } from './pluginHelpers';
+import t from '../locale/i18n';
 
 export default function createExternalPlugin() {
   const fetchResource = (embed, headers) => fetchOembed(embed, headers);
+
+  const onError = (embed, locale) =>
+    wrapInFigure(`
+      <img
+        alt="${t(locale, 'external.error')}"
+        role="presentation"
+        src="https://placeholdit.imgix.net/~text?bg=EFF0F2&txtclr=777777&txtsize=50&txtfont=Source-Serif-Pro&txt=${t(
+          locale,
+          'external.error'
+        )}&w=1600&h=800"
+      />
+    `);
 
   const embedToHTML = embed => wrapInFigure(embed.oembed.html);
 
   return {
     resource: 'external',
+    onError,
     fetchResource,
     embedToHTML,
   };
