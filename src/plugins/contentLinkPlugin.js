a1f9d310467f8ecf5cd0fcee04cff3ff7b3c8d15
Jonas Natten <jonas.natten@knowit.no>
2018-02-06 10:39:44 +0100

    #879: contentLinkPlugin now fetches taxonomy
    contentLinkPlugin now has a fetchResource function that fetches
    taxonomyPath for the given article. Falls back to old path without
    taxonomy if not found.

--- src/plugins/contentLinkPlugin.js
+++ src/plugins/contentLinkPlugin.js
@@ -7,21 +7,45 @@
  */
 
 import { ndlaFrontendUrl } from '../config';
+import { fetchArticleResource } from '../api/taxonomyApi';
+import log from '../utils/logger';
 
 export default function createContentLinkPlugin() {
+  async function fetchResource(embed, accessToken, lang) {
+    try {
+      const resource = await fetchArticleResource(
+        embed.data.contentId,
+        accessToken,
+        lang
+      );
+
+      const urnPath = resource.path
+        .split('/')
+        .map(s => (s !== '' ? `urn:${s}` : s))
+        .join('/');
+      const fullPath = `article${urnPath}/${embed.data.contentId}`;
+
+      return { ...embed, path: fullPath };
+    } catch (error) {
+      log.error(error);
+      return { ...embed, path: `article/${embed.data.contentId}` };
+    }
+  }
+
   const embedToHTML = (embed, lang) => {
     if (embed.data.openIn === 'new-context') {
-      return `<a href="${ndlaFrontendUrl}/${lang}/article/${
-        embed.data.contentId
+      return `<a href="${ndlaFrontendUrl}/${lang}/${
+        embed.path
       }" target="_blank" rel="noopener noreferrer">${embed.data.linkText}</a>`;
     }
-    return `<a href="${ndlaFrontendUrl}/${lang}/article/${
-      embed.data.contentId
-    }">${embed.data.linkText}</a>`;
+    return `<a href="${ndlaFrontendUrl}/${lang}/${embed.path}">${
+      embed.data.linkText
+    }</a>`;
   };
 
   return {
     resource: 'content-link',
+    fetchResource,
     embedToHTML,
   };
 }
