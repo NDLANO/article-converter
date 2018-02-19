ee4188f95c67ced4c8cd8df8bb57f5d6b0337a80
Torgeir Haugen <torgeirhaugen@gmail.com>
2018-02-19 15:29:11 +0100

    Use fallback=true, remove 404 handling

--- src/api/articleApi.js
+++ src/api/articleApi.js
@@ -11,27 +11,20 @@ import {
   apiResourceUrl,
   resolveJsonOrRejectWithError,
   headerWithAccessToken,
-  rejectWithError,
 } from '../utils/apiHelpers';
 
 export async function fetchArticle(articleId, accessToken, language) {
   const fetchit = async lang =>
     fetch(
-      apiResourceUrl(`/article-api/v2/articles/${articleId}?language=${lang}`),
+      apiResourceUrl(
+        `/article-api/v2/articles/${articleId}?language=${lang}&fallback=true`
+      ),
       {
         method: 'GET',
         headers: headerWithAccessToken(accessToken),
       }
     );
 
-  let response = await fetchit(language);
-  if (response.status === 404) {
-    const json = await response.json();
-    if (json.supportedLanguages) {
-      response = await fetchit(json.supportedLanguages[0]);
-    } else {
-      throw rejectWithError(json, response);
-    }
-  }
+  const response = await fetchit(language);
   return resolveJsonOrRejectWithError(response);
 }
