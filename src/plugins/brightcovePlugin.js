7fab8375af029fb61cbb7f82fa449d2ddf66cdc7
Sebastian Jara <sebastian.jara@knowit.no>
2018-06-18 14:20:05 +0200

    Fixed Brightcove plugin

--- src/plugins/brightcovePlugin.js
+++ src/plugins/brightcovePlugin.js
@@ -95,9 +95,11 @@ export default function createBrightcovePlugin() {
       source: t(locale, 'source'),
     };
 
-    const figureLicenseDialogId = `brightcove-${videoid}`;
+    const id = brightcove.reference_id;
+    const figureId = `figure-${id}`;
+
     return renderToStaticMarkup(
-      <Figure resizeIframe>
+      <Figure id={figureId} resizeIframe>
         <iframe
           title={`Video: ${brightcove.name}`}
           frameBorder="0"
@@ -105,14 +107,16 @@ export default function createBrightcovePlugin() {
           allowFullScreen
         />
         <FigureCaption
-          id={figureLicenseDialogId}
+          figureId={figureId}
+          id={id}
           caption={caption}
           reuseLabel={t(locale, 'video.reuse')}
           licenseRights={license.rights}
           authors={authors}
         />
         <FigureLicenseDialog
-          id={figureLicenseDialogId}
+          id={id}
+          title={brightcove.name}
           license={license}
           authors={contributors}
           messages={messages}>
