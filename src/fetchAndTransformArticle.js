6057ed0ecafb29227769629e0f91f840cece0b0f
Christian Peter <christian150790@gmail.com>
2018-07-27 13:05:34 +0200

    Adds new endpoint for posting a article and get a transformed article in return #1088

--- src/fetchAndTransformArticle.js
+++ src/fetchAndTransformArticle.js
@@ -2,29 +2,31 @@ import cheerio from 'cheerio';
 import { fetchArticle } from './api/articleApi';
 import { transform } from './transformers';
 
+export async function transformArticle(article, lang, accessToken) {
+  const articleContent = article.content.content
+    ? cheerio.load(article.content.content)
+    : undefined;
+  const { html, embedMetaData } = articleContent
+    ? await transform(articleContent, lang, accessToken, article.visualElement)
+    : {};
+
+  return {
+    ...article,
+    content: html || '',
+    metaData: embedMetaData || '',
+    title: article.title.title || '',
+    tags: article.tags.tags,
+    introduction: article.introduction ? article.introduction.introduction : '',
+    metaDescription: article.metaDescription.metaDescription || '',
+  };
+}
+
 export default async function fetchAndTransformArticle(
   articleId,
   lang,
   accessToken
 ) {
   const article = await fetchArticle(articleId, accessToken, lang);
-  const articleContent = cheerio.load(article.content.content);
-  const { html, embedMetaData } = await transform(
-    articleContent,
-    lang,
-    accessToken,
-    article.visualElement
-  );
-
-  return {
-    ...article,
-    content: html,
-    metaData: embedMetaData,
-    title: article.title.title,
-    tags: article.tags.tags,
-    introduction: article.introduction
-      ? article.introduction.introduction
-      : undefined,
-    metaDescription: article.metaDescription.metaDescription,
-  };
+  const transformedArticle = await transformArticle(article, accessToken, lang);
+  return transformedArticle;
 }
