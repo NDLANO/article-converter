e130c4cbab8262acb1223b3d02e9bdd7e913f799
Christian Peter <christian150790@gmail.com>
2019-01-03 14:59:56 +0100

    Adds updated ndla/ui

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,5 @@ module.exports = {
   testRegex: '/__tests__/.*-test.(js|jsx)$',
   setupFiles: ['./src/__tests__/_initTestEnv.js'],
   testEnvironment: 'node',
+  snapshotSerializers: ['jest-emotion/serializer'],
 };
