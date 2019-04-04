c918ffab100115e73007d9304897eeedf36cf8b7
Alireza Balouch <alireza.balouch@knowit.no>
2019-04-04 15:44:17 +0200

    use getMetaData from external plugins to fetch h5p copyright data if it is external type

--- src/plugins/externalPlugin.js
+++ src/plugins/externalPlugin.js
@@ -11,6 +11,7 @@ import { fetchOembed } from '../api/oembedProxyApi';
 import { wrapInFigure, errorSvgSrc } from './pluginHelpers';
 import t from '../locale/i18n';
 import { render } from '../utils/render';
+import { fetchH5p } from '../api/h5pApi'
 
 export default function createExternalPlugin() {
   const fetchResource = (embed, headers) => fetchOembed(embed, headers);
@@ -25,10 +26,25 @@ export default function createExternalPlugin() {
 
   const embedToHTML = embed => wrapInFigure(embed.oembed.html);
 
+  const getMetaData = embed => {
+    // TODO: build the structure like the one we need in graphql type "copyright"
+    if(embed && embed.data && embed.data.resource && embed.data.resource === 'external' && embed.data.url ){
+      console.log('h5p url' , embed.data.url);
+      let arrUrl = embed.data.url.split('/');
+      let h5pID = (arrUrl && arrUrl.length && arrUrl[arrUrl.length -1])?arrUrl[arrUrl.length -1] : false;
+      if(h5pID){
+        let content = fetchH5p(h5pID);
+        console.log(content);
+      }
+    }
+    return {};
+  };
+
   return {
     resource: 'external',
     onError,
     fetchResource,
     embedToHTML,
+    getMetaData
   };
 }
