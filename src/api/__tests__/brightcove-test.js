ff0ed0d3d6f46c6c09559d18d7d104ba15d697e3
Gunnar Velle <gunnar.velle@gmail.com>
2020-02-27 09:02:48 +0100

    Update test

--- src/api/__tests__/brightcove-test.js
+++ src/api/__tests__/brightcove-test.js
@@ -2,8 +2,8 @@ import { getContributorGroups } from '../brightcove';
 
 test('parses contributor string correctly', () => {
   const fields = {
-    licenseinfo: 'Opphavsmann: Senter for nye medier, Høgskolen i Bergen',
-    licenseinfo1: 'Opphavsmann:Adalia film & media',
+    licenseinfo: 'Opphaver: Senter for nye medier, Høgskolen i Bergen',
+    licenseinfo1: 'Opphaver:Adalia film & media',
     licenseinfo2: 'Leverandør:    Norsk Helseinformatikk',
     licenseinfo3: 'Distributør : ndla.no',
     yt_privacy_status: 'private',
@@ -16,9 +16,9 @@ test('parses contributor string correctly', () => {
 
 test('uses fallback if contributor string could not be parsed', () => {
   const fields = {
-    licenseinfo: 'Opphavsmann: Senter for nye medier, Høgskolen i Bergen',
-    licenseinfo1: 'Opphavsmann Adalia film & media',
-    licenseinfo2: ' Opphavsmann: Adalia film & media',
+    licenseinfo: 'Opphaver: Senter for nye medier, Høgskolen i Bergen',
+    licenseinfo1: 'Opphaver Adalia film & media',
+    licenseinfo2: ' Opphaver: Adalia film & media',
     licenseinfo3: 'Produsent: Lucasfilm',
     yt_privacy_status: 'private',
     license: 'Navngivelse-Del på samme vilkår',
