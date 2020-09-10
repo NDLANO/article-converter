5039e7d58721fe99485a4217a3fa01d676cdbc89
Gunnar Velle <gunnar.velle@gmail.com>
2020-09-10 08:38:36 +0200

    Bump packages. Remove code moved to h5p-plugin.

--- src/plugins/externalPlugin.js
+++ src/plugins/externalPlugin.js
@@ -11,40 +11,13 @@ import { fetchOembed } from '../api/oembedProxyApi';
 import { wrapInFigure, errorSvgSrc } from './pluginHelpers';
 import t from '../locale/i18n';
 import { render } from '../utils/render';
-import { fetchH5pLicenseInformation } from '../api/h5pApi';
 
 export default function createExternalPlugin() {
   const fetchResource = (embed, accessToken) =>
     new Promise((resolve, reject) => {
       fetchOembed(embed, accessToken)
-        .then(data => data)
         .then(data => {
-          if (data && data.embed && data.embed.data) {
-            const myData = data.embed.data();
-            let h5pID = false;
-            if (myData.url) {
-              const arrUrl = myData.url.split('/');
-              // making sure we don't search for non hp5 urls
-              if (arrUrl[2].includes('h5p')) {
-                h5pID =
-                  arrUrl && arrUrl.length && arrUrl[arrUrl.length - 1]
-                    ? arrUrl[arrUrl.length - 1]
-                    : false;
-              }
-            }
-
-            if (h5pID) {
-              fetchH5pLicenseInformation(h5pID)
-                .then(h5pData => {
-                  h5pData.url = myData.url;
-                  data.embed.h5p = h5pData;
-                  resolve(data);
-                })
-                .catch(() => resolve(data));
-            } else {
-              return resolve(data);
-            }
-          }
+          return resolve(data);
         })
         .catch(reject);
     });
@@ -59,19 +32,10 @@ export default function createExternalPlugin() {
 
   const embedToHTML = embed => wrapInFigure(embed.oembed.html);
 
-  const getMetaData = embed => {
-    if (embed && embed.embed && embed.embed.h5p) {
-      return embed.embed.h5p;
-    } else {
-      return null;
-    }
-  };
-
   return {
     resource: 'external',
     onError,
     fetchResource,
     embedToHTML,
-    getMetaData,
   };
 }
