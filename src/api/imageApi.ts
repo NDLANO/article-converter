4ce295cbf9cd987026271b29cfbfe4e00aa7ee8d
Jonas Natten <jonas.natten@knowit.no>
2021-09-28 11:27:26 +0200

    #2768: Converts imageApi to typescript

--- src/api/imageApi.ts
+++ src/api/imageApi.ts
@@ -12,14 +12,65 @@ import {
   resolveJsonOrRejectWithError,
   convertToInternalUrlIfPossible,
 } from '../utils/apiHelpers';
+import { Author, EmbedType, LocaleType } from '../interfaces';
 
-export const fetchImageResources = async (embed, accessToken, language) => {
-  const response = await fetch(
-    `${convertToInternalUrlIfPossible(embed.data.url)}?language=${language}`,
-    {
-      headers: headerWithAccessToken(accessToken),
-    },
-  );
-  const image = await resolveJsonOrRejectWithError(response);
+interface ImageApiCopyright {
+  license: {
+    license: string;
+    description: string;
+    url?: string;
+  };
+  origin: string;
+  creators: Author[];
+  processors: Author[];
+  rightsholders: Author[];
+  agreementId?: number;
+  validFrom?: string;
+  validTo?: string;
+}
+
+interface ImageApiType {
+  id: string;
+  metaUrl: string;
+  title: {
+    title: string;
+    language: string;
+  };
+  alttext: {
+    alttext: string;
+    language: string;
+  };
+  imageUrl: string;
+  size: number;
+  copyright: ImageApiCopyright;
+  tags: {
+    tags: string[];
+    language: string;
+  };
+  caption: {
+    caption: string;
+    language: string;
+  };
+  supportedLanguages: string[];
+  created: string;
+  createdBy: string;
+  modelRelease: string;
+  editorNotes?: {
+    timestamp: string;
+    updatedBy: string;
+    note: string;
+  }[];
+}
+
+export const fetchImageResources = async (
+  embed: EmbedType,
+  accessToken: string,
+  language: LocaleType,
+): Promise<EmbedType & { image: ImageApiType }> => {
+  const url = typeof embed.data.url === 'string' ? embed.data.url : '';
+  const response = await fetch(`${convertToInternalUrlIfPossible(url)}?language=${language}`, {
+    headers: headerWithAccessToken(accessToken),
+  });
+  const image = await resolveJsonOrRejectWithError<ImageApiType>(response);
   return { ...embed, image };
 };
