6205ce3a92804fbf6d8939eaa4a573c08065830c
Jonas Natten <jonas.natten@knowit.no>
2021-09-29 13:56:52 +0200

    #2768: Adds typescript file to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testRegex: '.*src.*/__tests__/.*-test.(js|jsx)$',
+  testRegex: '.*src.*/__tests__/.*-test.(js|jsx|ts|tsx)$',
   setupFiles: ['./src/__tests__/_initTestEnv.js'],
   testEnvironment: 'node',
   snapshotSerializers: ['@emotion/jest/serializer'],
